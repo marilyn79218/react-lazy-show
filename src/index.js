ce512779a684dfca3815ed550cdee734bfa8d500
ray8863 <marilyn79218@gmail.com>
2018-04-17 00:10:41 +0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import SelectableGroup from './SelectableGroup';
-import createSeletable from './createSeletable';
-import LazyShowHOC from './LazyShowHOC';
-
-export {
-  SelectableGroup,
-  createSeletable,
-  LazyShowHOC,
-};
