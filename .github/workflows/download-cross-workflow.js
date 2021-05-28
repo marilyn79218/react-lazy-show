96d9fa30fa653eb2b9f6cefc22cfd4159254834b
ray.lei <ray.lei@thecarousell.com>
2021-05-28 18:32:08 +0800

    Update comments.

--- .github/workflows/download-cross-workflow.js
+++ .github/workflows/download-cross-workflow.js
@@ -12,11 +12,12 @@ const path = require("path");
 // Ref: https://github.com/actions/download-artifact#download-all-artifacts
 
 // Case 1:
-// 綜合 (A) & (B), 我們的 actifact 路徑為 project_root_directory/artifact
+// 當 "upload-artifact@v2" 沒有設定 name，
+// 則綜合 (A) & (B), 我們的 actifact 路徑為 project_root_directory/artifact
 // const actifactFolderPath = path.join(process.cwd(), "artifact");
 
 // Case 2:
-// 如果今天 "upload-artifact@v2" 有設定 name 為 "uploaded-artifact"
+// 如果今天 "upload-artifact@v2" 有設定 name 為 "uploaded-artifact"，
 // 則我們的 actifact 路徑為 project_root_directory/uploaded-artifact
 const actifactFolderPath = path.join(process.cwd(), "uploaded-artifact");
 
