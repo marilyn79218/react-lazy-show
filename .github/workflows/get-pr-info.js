e210037f1b5a14d13eb4dcb8d254703e042507f2
marilyn79218 <marilyn79218@gmail.com>
2021-05-28 22:02:25 +0800

    Get PR number. (#28)

---
+++ .github/workflows/get-pr-info.js
@@ -0,0 +1,21 @@
+
+module.exports = async (github, context, core) => {
+  // Get PR associated with commit hash
+  const listPrResponse = await github.repos.listPullRequestsAssociatedWithCommit(
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commit_sha: process.env.GITHUB_SHA,
+    },
+  );
+
+  if (listPrResponse.data.length === 0) {
+    // Should not happen, since all our commits to master are via merged PRs.
+    console.log("Commit hash not associated with any PR");
+    return;
+  }
+  
+  const prInfo = listPrResponse.data[0];
+
+  core.setOutput("prInfo", prInfo);
+};
