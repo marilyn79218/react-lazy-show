e1a77e12f45af819d92842772dfb1bb78ebb021a
ray.lei <ray.lei@thecarousell.com>
2021-05-28 16:58:35 +0800

    Investigate which exactly the file we downloaded.

--- .github/workflows/download-cross-workflow.js
+++ .github/workflows/download-cross-workflow.js
@@ -5,12 +5,12 @@ const path = require("path");
 const actifactFolderPath = path.join(process.cwd());
 fs.readdirSync(actifactFolderPath, "utf-8").forEach(file => {
   if (file.endsWith(".txt")) {
-    // console.log("file", file);
+    console.log("file", file);
 
     const rawFileData = fs.readFileSync(path.join(actifactFolderPath, file), "utf-8");
     console.log("rawFileData", rawFileData);
 
-    // const fileData = JSON.parse(rawFileData);
-    // console.log("fileData: ", fileData);
+    const fileData = JSON.parse(rawFileData);
+    console.log("fileData: ", fileData);
   }
 });
