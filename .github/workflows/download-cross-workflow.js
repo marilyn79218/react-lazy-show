45ed0bea62c8227d92c9799be65755ccdb2ee5d8
ray.lei <ray.lei@thecarousell.com>
2021-05-28 13:17:11 +0800

    Try upload & download artifact.

---
+++ .github/workflows/download-cross-workflow.js
@@ -0,0 +1,16 @@
+// @noflow
+const fs = require("fs");
+const path = require("path");
+
+const actifactFolderPath = path.join(process.cwd());
+fs.readdirSync(actifactFolderPath, "utf-8").forEach(file => {
+  if (file.endsWith(".txt")) {
+    // console.log("file", file);
+
+    const rawFileData = fs.readFileSync(path.join(actifactFolderPath, file), "utf-8");
+    console.log("rawFileData", rawFileData);
+
+    // const fileData = JSON.parse(rawFileData);
+    // console.log("fileData: ", fileData);
+  }
+});
