4fbf61828aec3de9db992c749603e610944a1471
marilyn79218 <marilyn79218@gmail.com>
2021-07-05 18:56:10 +0800

    refactor: [dependabot write permission] An independent folder (#61)

---
+++ .github/workflows/pr-dependabot/utils.js
@@ -0,0 +1,47 @@
+// @noflow
+const fs = require("fs");
+const path = require("path");
+
+const getPrInfo = async (github, context, core, commitHash) => {
+  const hash = commitHash || process.env.GITHUB_SHA;
+
+  // Get PR associated with commit hash
+  const listPrResponse = await github.repos.listPullRequestsAssociatedWithCommit(
+    {
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commit_sha: hash,
+    },
+  );
+
+  if (listPrResponse.data.length === 0) {
+    // Should not happen, since all our commits to master are via merged PRs.
+    console.log("Commit hash not associated with any PR");
+    return;
+  }
+  
+  const prInfo = listPrResponse.data[0];
+  return prInfo;
+};
+
+// Read artifact content that was uploaded in previous action
+const readFileFromArtifact = () => {
+  try {
+    let fileData;
+    const actifactFolderPath = path.join(process.cwd());
+    fs.readdirSync(actifactFolderPath, "utf-8").forEach(file => {
+      if (file.endsWith(".txt")) {
+        const rawFileData = fs.readFileSync(
+          path.join(actifactFolderPath, file),
+          "utf-8",
+        );
+        fileData = JSON.parse(rawFileData);
+      }
+    });
+
+    return fileData;
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
---
+++ .github/workflows/pr-dependabot/writing-comments.js
@@ -0,0 +1,59 @@
+const {
+  getPrInfo,
+  readFileFromArtifact,
+} = require("./utils");
+
+const COMMENT_ANCHOR = "dependabot_comments";
+
+// Allowing contributor AND dependabot to write comments to a pull request
+module.exports = async (github, context, core, commitHash, workflow) => {
+  try {
+    const fileData = readFileFromArtifact();
+
+    const prInfo = await getPrInfo(github, context, core, commitHash);
+    const prNumber = prInfo.number;
+
+    console.log("prNumber", prNumber);
+    console.log("data", fileData);
+    console.log("context", context);
+    // TODO: curious about the workflow object
+    console.log("workflow", workflow);
+
+    // Find comment id if exist
+    const { data: existingComments } = await github.issues.listComments({
+      issue_number: prNumber,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    });
+
+    let commentId;
+    for (let i = existingComments.length; i--; ) {
+      const c = existingComments[i];
+      if (c.user.type === "Bot" && c.body.includes(COMMENT_ANCHOR)) {
+        commentId = c.id;
+        break;
+      }
+    }
+
+    const commentBody = `${fileData} (${commitHash}) <sub>${COMMENT_ANCHOR}</sub>`;
+    if (!commentId) {
+      console.log("Creating comment...");
+      await github.issues.createComment({
+        issue_number: prNumber,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: commentBody,
+      });
+    } else {
+      console.log("Updating comment...", commentId);
+      await github.issues.updateComment({
+        comment_id: commentId,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: commentBody,
+      });
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
