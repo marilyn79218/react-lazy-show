6bfe68ccd9396c315b317b027fbf79db8754bb1b
ray.lei <ray.lei@thecarousell.com>
2021-07-07 23:08:46 +0800

    Set up permission for dependabot manually.

---
+++ .github/workflows/dependabot-comments/permission-key.js
@@ -0,0 +1,44 @@
+const COMMENT_ANCHOR = "dependabot_permission_key";
+
+module.exports = async (github, context, core) => {
+  try {
+    // Find comment id if exist
+    const { data: existingComments } = await github.issues.listComments({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    });
+
+    let commentId;
+    for (let i = existingComments.length; i--; ) {
+      const c = existingComments[i];
+      if (c.user.type === "Bot" && c.body.includes(COMMENT_ANCHOR)) {
+        commentId = c.id;
+        break;
+      }
+    }
+
+    const date = new Date();
+    const localTimeString = date.toLocaleString();
+    const commentBody = `${COMMENT_ANCHOR}: ${localTimeString}`;
+    if (!commentId) {
+      console.log("Creating comment...");
+      await github.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: commentBody,
+      });
+    } else {
+      console.log("Updating comment...", commentId);
+      await github.issues.updateComment({
+        comment_id: commentId,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: commentBody,
+      });
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
