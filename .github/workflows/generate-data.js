22af033f00d6651987e6f3ff0607cf591433c50c
ray.lei <ray.lei@thecarousell.com>
2021-07-13 13:58:05 +0800

    Try array data again

--- .github/workflows/generate-data.js
+++ .github/workflows/generate-data.js
@@ -2,14 +2,21 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = async (github, context, core, filename) => {
-  const fileContent = JSON.stringify({
-    map: {
-      filename,
-      a: "Abc",
-      b: "aBc",
-      c: "abC"
-    }
-  });
+  const data = [];
+  for (let i=0; i<3; i++) {
+    data.push(`idx: ${i} - time: ${new Date().toISOString()}`);
+  };
+
+  const fileContent = JSON.stringify(data);
+
+  // const fileContent = JSON.stringify({
+  //   map: {
+  //     filename,
+  //     a: "Abc",
+  //     b: "aBc",
+  //     c: "abC"
+  //   }
+  // });
 
   const filePath = path.join(process.cwd(), filename);
 
