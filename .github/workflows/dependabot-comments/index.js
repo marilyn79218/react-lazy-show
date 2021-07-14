a432d3d9dad15a75d85d05edfbfde6e2a3406755
marilyn79218 <marilyn79218@gmail.com>
2021-07-14 15:25:38 +0800

    Add log (#96)

--- .github/workflows/dependabot-comments/index.js
+++ .github/workflows/dependabot-comments/index.js
@@ -12,6 +12,7 @@ module.exports = async (github, context, core, commitHash) => {
     const secondFileData = readFileFromArtifact("second-data-arctifact");
     const fileData = firstFileData.concat(secondFileData);
 
+    console.log("context issue", context.issue);
     console.log("context number", context.issue.number);
 
     const prInfo = await getPrInfo(github, context, core, commitHash);
