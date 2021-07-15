9664e3b99c07752e97ca4cc1da17ab22d644040f
marilyn79218 <marilyn79218@gmail.com>
2021-07-15 18:46:24 +0800

    experiment: [Logging master info] Log master branch (#103)

--- .github/workflows/get-pr-info.js
+++ .github/workflows/get-pr-info.js
@@ -1,23 +1,11 @@
+const { getPrInfo } = require("./dependabot-comments/utils");
 
-module.exports = async (github, context, core, commitHash) => {
-  const hash = commitHash || process.env.GITHUB_SHA;
+module.exports = async (github, context, core) => {
+  const commitHash = process.env.GITHUB_SHA;
+  console.log("commit hash", commitHash);
 
-  // Get PR associated with commit hash
-  const listPrResponse = await github.repos.listPullRequestsAssociatedWithCommit(
-    {
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      commit_sha: hash,
-    },
-  );
+  const prInfo = await getPrInfo(github, context, core, commitHash);
 
-  if (listPrResponse.data.length === 0) {
-    // Should not happen, since all our commits to master are via merged PRs.
-    console.log("Commit hash not associated with any PR");
-    return;
-  }
-  
-  const prInfo = listPrResponse.data[0];
   console.log("get-pr-info", prInfo);
 
   core.setOutput("prNumber", prInfo.number);
